008cd56ce7da1de02f169ac62df575e5b1e4da92
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-06-06 13:18:56 +0100

    Migrando la web a Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/header/header';
+import { Hero } from './sections/hero/hero';
+import { Projects } from './sections/projects/projects';
+import { Skills } from './sections/skills/skills';
+import { Experience } from './sections/experience/experience';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Hero, Projects, Skills, Experience],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
